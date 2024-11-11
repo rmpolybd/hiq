cbb5335c2d6dc691046daa5ad8d023f32a21cff3
rmpolybd <rmpolybd@gmail.com>
2024-11-11 11:38:15 +0000

    ddd

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(".box") .click (function () {
     $ (this) .addClass ("active");
 });
 
+
 $(window).scroll(function() {
 
     if ($(this).scrollTop() > 190){  
@@ -12,3 +13,35 @@ $(window).scroll(function() {
         $('#nav').removeClass("fixed");
     }
 });
+
+/* Feature Slider*/
+const wrapper = document.querySelector('.blog-slider__wrp');
+const bullets = document.querySelectorAll('.bullet');
+const items = document.querySelectorAll('.blog-slider__item');
+let currentSlide = 0;
+
+document.querySelector('.next').addEventListener('click', () => {
+  currentSlide = (currentSlide + 1) % items.length;
+  updateSlider();
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+  currentSlide = (currentSlide - 1 + items.length) % items.length;
+  updateSlider();
+});
+
+bullets.forEach((bullet, index) => {
+  bullet.addEventListener('click', () => {
+    currentSlide = index;
+    updateSlider();
+  });
+});
+
+function updateSlider() {
+  wrapper.style.transform = `translateX(-${currentSlide * 100}%)`;
+  bullets.forEach((bullet, index) => {
+    bullet.classList.toggle('active', index === currentSlide);
+  });
+}
+
+updateSlider();
